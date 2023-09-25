1a93eee6c8e7b2d0beb8d0aab4f4c33e22bd449c
klikflux <118182684+AlbertLindgren@users.noreply.github.com>
2023-09-25 09:36:48 +0200

    stuff

--- ImageEncryptor/ImageEncryptor/main.cpp
+++ ImageEncryptor/ImageEncryptor/main.cpp
@@ -1,5 +1,4 @@
-
-
+#include <iostream>
 // Put this last:
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -7,5 +6,11 @@
 
 int main()
 {
-
+	int width, height, channels;
+	unsigned char *image = stbi_load("img/test1.png", &width, &height, &channels, 3);
+	if (image == NULL) {
+		std::cout << "Error in loading the image\n";
+		exit(1);
+	}
+	std::cout << "Loaded image with a width of: " << width << "and a height of: " << height << " and " << channels << "channels" << std::endl;
 }
