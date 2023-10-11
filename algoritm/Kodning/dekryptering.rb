1250163bb5868eeb19541072bc8dac6680a25b47
Elias_Johannisson_NTI <elias.johannisson@elev.ga.ntig.se>
2023-10-11 16:26:01 +0200

    updating only dekryptering

--- algoritm/Kodning/dekryptering.rb
+++ algoritm/Kodning/dekryptering.rb
@@ -1,20 +1,14 @@
-char_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",".",","," ","?","!", nil]
-message = ""
-run = 0
-while run < 18
-    value = rand(0...5)
+def dekrypt(colors, message)
     char_val = []
-    char_val[0] = rand(0...2)%2
-    char_val[1] = rand(0...2)%2
-    char_val[2] = rand(0...2)%2
-    char_val[3] = rand(0...2)%2
-    char_val[4] = rand(0...2)%2
-    character = char_val[0]*1 + char_val[1]*2 + char_val[2]*4 + char_val[3]*8 + char_val[4]*16 
-    if character == 31
-        break
+    char_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",".",","," ","?","!", nil]
+    for x in 0..4
+        char_val[x] = colors[x]%2
     end
+    character = char_val[4]*1 + char_val[3]*2 + char_val[2]*4 + char_val[1]*8 + char_val[0]*16 
+    if character == 31
+        return message
+    end
+    p character
+    p char_list[character]
     message = message + char_list[character]
-    run += 1
 end
-
-puts message
