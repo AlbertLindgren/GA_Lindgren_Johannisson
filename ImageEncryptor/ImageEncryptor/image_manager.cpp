a0769f63a34bbf71bc836f86818208278e33da3f
klikflux <42775673+Madtail@users.noreply.github.com>
2023-10-14 17:10:57 +0200

    Prototyp saker

--- ImageEncryptor/ImageEncryptor/image_manager.cpp
+++ ImageEncryptor/ImageEncryptor/image_manager.cpp
@@ -1,19 +1,40 @@
 #include "image_manager.h"
-#include <iostream>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#include "stb_image_write.h"
 
 ImageManager::ImageManager()
 {
-	
-
+	_rgb_pixel = { 0, 0, 0 };
 }
 
 ImageManager::~ImageManager()
 {
-	stbi_image_free(img);
+	stbi_image_free(_img);
 }
 
-int ImageManager::importPixel()
+unsigned char* ImageManager::loadImage(const char* path)
+{
+	_img = stbi_load(path, &_width, &_height, &_channels, _channel_num);
+	return _img;
+}
+
+std::vector<int> ImageManager::importPixel(int nPixel)
+{
+	int index = 3 * nPixel;
+	_rgb_pixel[0] = static_cast<int>(_img[index + 0]);
+	_rgb_pixel[1] = static_cast<int>(_img[index + 1]);
+	_rgb_pixel[2] = static_cast<int>(_img[index + 2]);
+	return _rgb_pixel;
+}
+
+void ImageManager::exportPixel(int nPixel, std::vector<int> colour)
+{
+
+	uint8_t* c_pixels = new uint8_t[_width * _height * _channel_num];
+	int index = 3 * nPixel;
+	for (int i = 0; i < (_width * _height * _channel_num); i++) {
+		c_pixels[i] = static_cast<int>(_img[i]);
+	}
+	
+	c_pixels[index] = colour[0];
+	c_pixels[index + 1] = colour[1];
+	c_pixels[index + 2] = colour[2];
+}
--- ImageEncryptor/ImageEncryptor/image_manager.h
+++ ImageEncryptor/ImageEncryptor/image_manager.h
@@ -1,10 +1,21 @@
 #ifndef IMAGE_MANAGER
 #define IMAGE_MANAGER
 
+#include <vector>
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "stb_image_write.h"
+
+
 /* Class containing functions for loading image, storing important information in variables which can be accessed with get methods
 * and functions for getting and changing pixels.
 Idea: Create object that holds and tracks the current values of the relevant variables. 
 Ruby program sends image path -> c++ function takes it in
+Idea for prototype:
+Take in image path for every function call and track pixels with integer from ruby loop. 
+Return pixel by using std::array. 
 */
 
 class ImageManager
@@ -15,21 +26,21 @@ public:
 	~ImageManager();
 
 	unsigned char* loadImage(const char* path); // returns img
-	int importPixel(unsigned char* img); // choose pixel
-	int exportPixel(unsigned char* img);
+	std::vector<int> importPixel(int nPixel); // choose pixel and get colour values
+	void exportPixel(int nPixel, std::vector<int> colour);
 
 	// Get functions
-	int getWidth();
-	int getHeight();
-	int getChannels();
+	//int getWidth();
+	//int getHeight();
+	//int getChannels();
 
 private:
-	unsigned char* img;
-	int width;
-	int height;
-	int channels;
-	int currentPixel;
-	int nPixel; // pixel number
+	unsigned char* _img;
+	int _width;
+	int _height;
+	int _channels;
+	std::vector<int> _rgb_pixel;
+	const int _channel_num = 3;
 };
 
 #endif // IMAGE_MANAGER
